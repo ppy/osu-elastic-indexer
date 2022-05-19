ccc9a6185b84d8ff0706e68be4528243f8e7672d
bakaneko <notbakaneko@users.noreply.github.com>
2022-05-19 19:41:51 +0900

    handle missing user

--- osu.ElasticIndexer/Commands/PumpAllScores.cs
+++ osu.ElasticIndexer/Commands/PumpAllScores.cs
@@ -24,7 +24,7 @@ namespace osu.ElasticIndexer.Commands
                 var users = User.FetchUserMappings(scores);
                 foreach (var score in scores)
                 {
-                    score.country_code = users[score.user_id].country_acronym;
+                    score.country_code = users.ContainsKey(score.user_id) ? users[score.user_id].country_acronym : "XX";
                     Console.WriteLine($"Pushing {score}");
                     Processor.PushToQueue(new ScoreItem(score));
                 }
