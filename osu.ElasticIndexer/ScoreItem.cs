34af8ac0a3882e638d1aa76fcab61ddd905b3186
Dean Herbert <pe@ppy.sh>
2022-07-20 18:33:07 +0900

    Simplify `ScoreItem` construction

--- osu.ElasticIndexer/ScoreItem.cs
+++ osu.ElasticIndexer/ScoreItem.cs
@@ -1,25 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Runtime.Serialization;
 using osu.Server.QueueProcessor;
 
 namespace osu.ElasticIndexer
 {
     public class ScoreItem : QueueItem
     {
-        public long? ScoreId { get; set; }
+        public long? ScoreId { get; init; }
 
-        public SoloScore? Score { get; set; }
-
-        public ScoreItem()
-        {
-        }
-
-        public ScoreItem(SoloScore score)
-        {
-            Score = score;
-        }
+        // ScoreId is always preferred if present (this property is ignored).
+        // Note that this is generally not used anymore. Consider removing in the future unless a use case comes up?
+        public SoloScore? Score { get; init; }
 
         public override string ToString() => Score != null ? $"ScoreItem Score: {Score.id}" : $"ScoreItem ScoreId: {ScoreId}";
     }
