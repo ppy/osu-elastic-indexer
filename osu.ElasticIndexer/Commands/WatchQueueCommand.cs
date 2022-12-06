bf3eef0a85c0dd252d2c223808fe9497d2abcedb
Dean Herbert <pe@ppy.sh>
2022-12-06 15:35:55 +0900

    Remove `Wait` parameter
    If this is needed, I think it should be defined in a `docker-compose`
    file similar to how it was done over
    [here](https://github.com/ppy/osu-queue-score-statistics/blob/master/docker-compose.yml).

--- osu.ElasticIndexer/Commands/WatchQueueCommand.cs
+++ osu.ElasticIndexer/Commands/WatchQueueCommand.cs
@@ -3,24 +3,15 @@
 
 using System;
 using System.Threading;
-using Elasticsearch.Net;
 using McMaster.Extensions.CommandLineUtils;
-using StackExchange.Redis;
-using MySqlConnector;
 
 namespace osu.ElasticIndexer.Commands
 {
     [Command("queue", Description = "Watches queue and dispatches scores for indexing")]
     public class WatchQueueCommand
     {
-        [Option("--wait", Description = "Waits for dependent services to start.")]
-        public bool Wait { get; set; }
-
         public int OnExecute(CancellationToken token)
         {
-            if (Wait)
-                waitForServices();
-
             var schema = new Redis().GetSchemaVersion();
 
             if (string.IsNullOrEmpty(schema))
@@ -34,87 +25,6 @@ namespace osu.ElasticIndexer.Commands
             new SoloScoreIndexer().Run(token);
 
             return 0;
-        }
-
-        private void waitForServices()
-        {
-            waitForElasticsearch();
-            waitForRedis();
-            // Database is last because we want to isolate the error handling but we have to get the
-            // database connection string from QueueProcessor which also tries to connect to redis.
-            waitForDatabase();
-        }
-
-        // Only waits for the database, doesn't wait for tables or migrations to complete.
-        private void waitForDatabase()
-        {
-            while (true)
-            {
-                try
-                {
-                    // Can remove and use plain connection if https://github.com/ppy/osu-queue-processor/issues/13 is implemented.
-                    new UnrunnableProcessor().GetDatabaseConnection().Dispose();
-                    Console.WriteLine(ConsoleColor.Green, "Database is alive.");
-
-                    break;
-                }
-                catch (MySqlException ex)
-                {
-                    // die on any other kind of error, e.g. permissions, etc.
-                    if (ex.ErrorCode == MySqlErrorCode.UnableToConnectToHost || ex.ErrorCode == MySqlErrorCode.UnknownDatabase)
-                        Thread.Sleep(10);
-                    else
-                        throw;
-                }
-            }
-        }
-
-        private void waitForElasticsearch()
-        {
-            var client = new Client(false);
-
-            while (true)
-            {
-                try
-                {
-                    var response = client.ElasticClient.Cluster.Health();
-
-                    // Yellow or Green cluster statuses are fine.
-                    if (response.IsValid && response.Status != Health.Red)
-                    {
-                        Console.WriteLine(ConsoleColor.Green, $"Elasticsearch ({response.ClusterName}) is alive.");
-                        break;
-                    }
-                }
-                catch (UnexpectedElasticsearchClientException ex)
-                {
-                    // There is a period during elasticseasrch startup where active_shards_percent_as_number
-                    // is returned as NaN but the parser expects a number.
-                    if (!ex.Message.StartsWith("expected:'Number Token', actual:'\"NaN\"'", StringComparison.Ordinal))
-                        throw;
-                }
-
-                Thread.Sleep(10);
-            }
-        }
-
-        private void waitForRedis()
-        {
-            while (true)
-            {
-                try
-                {
-                    var redis = new Redis();
-                    redis.Connection.GetDatabase().Ping();
-                    Console.WriteLine(ConsoleColor.Green, $"Redis ({redis.Connection.Configuration}) is alive.");
-
-                    break;
-                }
-                catch (RedisConnectionException)
-                {
-                    Thread.Sleep(10);
-                }
-            }
         }
     }
 }
