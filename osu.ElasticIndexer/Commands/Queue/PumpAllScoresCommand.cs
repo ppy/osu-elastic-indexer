67cee7aa7deee74b2ead1d915b03436c3593673c
Dean Herbert <pe@ppy.sh>
2024-05-04 15:23:04 +0800

    Add flag to allow pumping unranked scores

--- osu.ElasticIndexer/Commands/Queue/PumpAllScoresCommand.cs
+++ osu.ElasticIndexer/Commands/Queue/PumpAllScoresCommand.cs
@@ -22,6 +22,9 @@ namespace osu.ElasticIndexer.Commands.Queue
         [Option("--switch", Description = "Update the configured schema in redis after completing.")]
         public bool Switch { get; set; }
 
+        [Option("--include-unranked", Description = "Pump scores with ranked flag set to false to trigger deletion")]
+        public bool IncludeUnranked { get; set; }
+
         private CancellationToken cancellationToken;
 
         private UnrunnableProcessor processor = null!;
@@ -66,7 +69,7 @@ namespace osu.ElasticIndexer.Commands.Queue
             using (var mySqlConnection = processor.GetDatabaseConnection())
 
             {
-                var chunks = ElasticModel.Chunk<Score>(mySqlConnection, "preserve = 1 AND ranked = 1", AppSettings.BatchSize, from);
+                var chunks = ElasticModel.Chunk<Score>(mySqlConnection, "preserve = 1 " + (IncludeUnranked ? string.Empty : "AND ranked = 1"), AppSettings.BatchSize, from);
                 Score? last = null;
 
                 foreach (var scores in chunks)
